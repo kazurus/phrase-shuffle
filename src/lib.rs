9c49015005bab6f68cda7c7750997df9d9075282
Alexander Kazimir <alex_kazik@mail.ru>
2019-11-19 15:28:33 +0300

    feat: change output format

--- src/lib.rs
+++ src/lib.rs
@@ -22,20 +22,24 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("In file {}", config.filename);
+    // println!("In file {}", config.filename);
 
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
-    println!("With text:/n{}", contents);
+    // println!("With text:/n{}", contents);
 
     let mut contents_list: Vec<_> = contents.lines().filter(|line| !line.is_empty()).collect();
 
-    println!("Lines: {:?}", contents_list);
+    // println!("Lines: {:?}", contents_list);
 
     contents_list.shuffle(&mut thread_rng());
 
-    println!("Lines after shuffle: {:?}", contents_list);
+    // println!("Lines after shuffle: {:?}", contents_list);
+
+    for line in contents_list {
+        println!("{}", line);
+    }
 
     Ok(())
 }
